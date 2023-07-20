9fd6a7307e7033eaf3e942d20ac78e85cfdcbc2e
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-07-20 12:41:01 +1000

    Added comments.

--- Sources/MemoryUtilities/MemoryManager.swift
+++ Sources/MemoryUtilities/MemoryManager.swift
@@ -56,14 +56,36 @@
 
 import Foundation
 
+/// A manager used to handle read and write operations to memory mapped files. This manager assumes 32-bit
+/// alignment for the data stores and should not be used if this is not the case. This manager supports read
+/// and write operations using 32-bit unsigned values.
 public final class MemoryManager {
 
-    let memory: UnsafeMutablePointer<UInt32>
+    /// A pointer to the memory-mapped region.
+    @usableFromInline let memory: UnsafeMutablePointer<UInt32>
 
+    /// The offset in bytes where user data begins. This value is always 32-bit aligned (i.e. multiples of 4)
+    /// and represents the first accessible address in the data store.
     public let baseAddress: size_t
 
+    /// The size of the data store in bytes. This value represents the number of bytes accessible to the user
+    /// from the `baseAddress`. The last valid address is located at `baseAddress + size - 4` using 32-bit
+    /// alignment. This value is always 32-bit aligned (i.e. multiples of 4) for this manager.
     public let size: size_t
 
+    /// Create a manager that uses a memory-mapped file for data storage.
+    /// - Parameters:
+    ///   - location: The location of the file to be memory-mapped. This file must exist and be a regular
+    /// file URL. This location will default to `/dev/mem`.
+    ///   - baseAddress: The start address within the file that is accessible to the user. For the entire file
+    /// to be accessible, this value should be 0. This value is always 32-bit aligned (i.e. multiples of 4).
+    ///   - size: The size in bytes of the memory-mapped region within the file. This size should be 32-bit
+    /// aligned and greater than 3 (atleast 4 bytes).
+    ///   - virtualAddress: The address where the memory-mapped region should be mapped to. The linux kernel
+    /// will choose a suitable address if this value is `nil`. There are no guarantees that the linux kernel
+    /// will use this address. The virtual address needs to be page-aligned (i.e. multiples of
+    /// `sysconf(_SC_PAGESIZE)`). It is more portable to keep this value as `nil`.
+    @inlinable
     public convenience init?(
         location: URL = URL(fileURLWithPath: "/dev/mem", isDirectory: false),
         baseAddress: off_t,
@@ -97,28 +119,60 @@ public final class MemoryManager {
         self.init(memory: memory, baseAddress: size_t(baseAddress), size: size)
     }
 
+    /// Initialise the stored properties of this manager.
+    /// - Parameters:
+    ///   - memory: A pointer to the memory-mapped region of this data store.
+    ///   - baseAddress: The offset where user data begins. This offset is relative to the `memory` base
+    /// address and is in bytes.
+    ///   - size: The number of bytes accessible to the user from the `baseAddress`. The last valid address
+    /// is located at `memory.baseAddress + baseAddress + size - 4`. Please make sure the size is greater than
+    /// 0 and 32-bit aligned (i.e. multiples of 4) before using this initialiser.
+    @inlinable
     init(memory: UnsafeMutablePointer<UInt32>, baseAddress: size_t, size: size_t) {
         self.memory = memory
         self.baseAddress = baseAddress
         self.size = size
     }
 
+    /// Remove the memory-mapped region.
+    @inlinable
     deinit {
         _ = munmap(UnsafeMutableRawPointer(memory), size)
     }
 
+    /// Check whether an address is valid for this data store.
+    /// - Parameter address: The address to validate.
+    /// - Returns: Whether the address is valid.
+    @inlinable
     func isValidAddress(address: size_t) -> Bool {
         address % 4 == 0 && address >= self.baseAddress && address < self.baseAddress + self.size - 3
     }
 
+    /// Reads a value located at an `address`.
+    /// - Parameter address: The address to read from. This address must be 32-bit aligned (i.e. a multiple
+    /// of 4).
+    /// - Returns: The value located at `address`.
+    /// - Warning: If the address is not 32-bit aligned, then this function will returned an undefined value.
+    @inlinable
     func performRead(address: size_t) -> UInt32 {
         self.memory[baseAddress + (address - baseAddress) / 4]
     }
 
+    /// Writes a `value` to an `address`.
+    /// - Parameters:
+    ///   - address: The address to write to. This address must be 32-bit aligned.
+    ///   - value: The value to write to the address.
+    /// - Warning: If the address is not 32-bit aligned, then this function will write to invalid memory.
+    @inlinable
     func performWrite(address: size_t, value: UInt32) {
         self.memory[baseAddress + (address - baseAddress) / 4] = value
     }
 
+    /// Read a value from an `address`.
+    /// - Parameter address: The address to read from. This address must be 32-bit aligned (i.e. a multiple
+    /// of 4) and represent the first byte of the value. This must also be valid in the memory-mapped region.
+    /// - Returns: The value located at this address or `nil` if the address is invalid.
+    @inlinable
     public func read(address: size_t) -> UInt32? {
         guard self.isValidAddress(address: address) else {
             return nil
@@ -126,6 +180,14 @@ public final class MemoryManager {
         return performRead(address: address)
     }
 
+    /// Read multiple values starting at an `address`.
+    /// - Parameters:
+    ///   - address: The address of the first byte in the sequence. This address must be 32-bit aligned and 
+    /// exist in the memory-mapped region. This address must also contain enough successors to read all
+    /// values. The read operation will fail before any data is read if this is not the case.
+    ///   - items: The number of `UInt32` values to read.
+    /// - Returns: An array of `UInt32` values or `nil` if the address is invalid.
+    @inlinable
     public func read(address: size_t, items: Int) -> [UInt32]? {
         guard
             self.isValidAddress(address: address + size_t(items * 4 - 4)),
@@ -136,6 +198,13 @@ public final class MemoryManager {
         return (0..<items).map { performRead(address: address + size_t($0 * 4)) }
     }
 
+    /// Write a `value` to an `address`.
+    /// - Parameters:
+    ///   - address: The address to write the value to. This address must be 32-bit aligned and exist within
+    /// the memory-mapped region.
+    ///   - value: The value to write to `address`.
+    /// - Returns: Whether the write operation was successful.
+    @discardableResult @inlinable
     public func write(address: size_t, value: UInt32) -> Bool {
         guard self.isValidAddress(address: address) else {
             return false
@@ -144,6 +213,14 @@ public final class MemoryManager {
         return true
     }
 
+    /// Write multiple `values` starting at an `address`.
+    /// - Parameters:
+    ///   - address: The address of the first byte to write to. This address must be 32-bit aligned and exist
+    /// within the memory-mapped region. The first address must also contain enough successors to write all
+    /// values. The write operation will fail before any data is written if this is not the case.
+    ///   - values: The values to write starting at `address`.
+    /// - Returns: Whether the write operation was successful.
+    @discardableResult @inlinable
     public func write(address: size_t, values: [UInt32]) -> Bool {
         guard
             self.isValidAddress(address: address + size_t(values.count * 4 - 4)),
